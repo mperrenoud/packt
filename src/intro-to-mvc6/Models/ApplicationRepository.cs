e92f75054c43ad3c0441cb031b1e7cf0ca42ccdf
Michael Perrenoud <info@consultwithmike.us>
2016-01-22 12:54:22 -0700

    Initial commit.

---
+++ src/intro-to-mvc6/Models/ApplicationRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace intro_to_mvc6.Models
+{
+    public class ApplicationRepository : IApplicationRepository
+    {
+        private ApplicationDbContext _context;
+
+        public ApplicationRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<ToDo> GetAllToDos()
+        {
+            return _context.ToDos.ToList();
+        }
+    }
+
+    public interface IApplicationRepository
+    {
+        List<ToDo> GetAllToDos();
+    }
+}
