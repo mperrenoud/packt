26c69743017d0e8a937d1d4f7b0c5c04b7531c0d
Michael Perrenoud <info@consultwithmike.us>
2016-01-27 05:54:41 -0700

    03_03

---
+++ src/intro-to-mvc6/Controllers/API/ToDoController.cs
@@ -0,0 +1,27 @@
+﻿using intro_to_mvc6.Models;
+using intro_to_mvc6.ViewModels;
+using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace intro_to_mvc6.Controllers.API
+{
+    [Route("api/[controller]")]
+    public class ToDoController : Controller
+    {
+        private IApplicationRepository _repository;
+
+        public ToDoController(IApplicationRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public JsonResult Get()
+        {
+            var todos = AutoMapper.Mapper.Map(this._repository.GetAllToDos(), new List<ToDoViewModel>());
+            return Json(todos);
+        }
+    }
+}
